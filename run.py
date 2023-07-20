61b8aefa4330b94c326f47a2bb02020766999dbd
D0nie1998 <115627417+D0nie1998@users.noreply.github.com>
2023-07-20 19:57:00 +0100

    main menu screen add

--- run.py
+++ run.py
@@ -30,3 +30,26 @@ def get_word():
     word = random.choice(word_list)
     return word.upper()
 
+def welcome_screen():
+    clear_terminal()
+    print("Let's")
+    print(" _ _ _ _ _ ____ _ ")
+    print(" | | | | / \ | \ | |/ __| __ ___ __ _ _ __ | |")
+    print(" | || | / _ \ | \| | | _| ' ` _ \ / ` | ' \ | | ")
+    print(" | _ |/ ___ \| |\ | |_| | | | | | | (_| | | | ||_|")
+    print(" |_| |_/_/ \_\_| \_|\____|_| |_| |_|\__,_|_| |_|(_)")
+    print()
+    print("\n" * 2)
+    print("{:^70}".format("1: PLAY GAME"))
+    print("{:^70}".format("3: EXIT"))
+    print("\n" * 3)
+
+    while True:
+        welcome_screen_choice = input("  " * 11 + "Please choose an option : ")
+        if welcome_screen_choice == "1":
+            player_name()
+        elif welcome_screen_choice == "3":
+            clear_terminal()
+            sys.exit()
+        else:
+            print("{:^70}".format("Please Choose option 1, 2 or 3"))
