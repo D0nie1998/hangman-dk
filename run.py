6550834fd1dfd5c68a0ba3ea8963828c421048d8
D0nie1998 <115627417+D0nie1998@users.noreply.github.com>
2023-07-20 20:08:47 +0100

    add if guessed work was right function

--- run.py
+++ run.py
@@ -112,3 +112,13 @@ def play(word, letters_box):
                 attempts -= 1
             else:
                 guessed = True
+
+     else:
+            if validate_guess(guess, guessed_letters):
+                guessed_letters.append(guess)
+                letters_box2 = letters_box2.replace(guess.upper(), '*')
+                if check_if_guess_in_word(guess, word):
+                    completed_word = replace_guess(word, completed_word, guess)
+                    print("Well done!", guess, "is in the word.")
+                    if completed_word.upper() == word.upper():
+                        guessed = True
