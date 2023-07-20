22d5a2b319e8477986a2790696de1a6b04e3489d
D0nie1998 <115627417+D0nie1998@users.noreply.github.com>
2023-07-20 19:56:07 +0100

    get word function added

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import sys
+import os
+import operator
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+from words import word_list
+
+
+
+game_results = {}
+
+current_word = ''
+masked_word = ''
+
+
+def clear_terminal():
+    """
+    Clears the terminal and code sourced from:
+    http://www.coding4you.at/inf_tag/beginners_python_cheat_sheet.pdf
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
